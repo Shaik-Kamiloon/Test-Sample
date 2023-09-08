d08a55385826078b70b3aef027ae7ab4a7b6d7c1
Kamiloon <kamiloon.shaik@solitontech.com>
2023-09-08 16:51:58 +0530

    added a test into the frequency calculator

--- test_char_frequency.py
+++ test_char_frequency.py
@@ -3,8 +3,10 @@ from char_frequency import FrequencyCalculator
 def test_when_string_given_char_frequency_return_frequency_dictionary():
     frequency_malayalam = FrequencyCalculator().char_frequency("malayalam")
     frequency_halocination = FrequencyCalculator().char_frequency("halocination")
+    frequency_madam = FrequencyCalculator().char_frequency("madam")
     assert frequency_malayalam == {'a': 4, 'l': 2, 'm': 2, 'y': 1}
     assert frequency_halocination == {'a': 2, 'c': 1, 'h': 1, 'i': 2, 'l': 1, 'n': 2, 'o': 2, 't': 1}
+    assert frequency_madam == {'a': 2, 'd': 1, 'm': 2}
 
 def test_when_numeric_string_given_return_frequency_dictionary():
     frequency_invalid_string = FrequencyCalculator().char_frequency("123")
